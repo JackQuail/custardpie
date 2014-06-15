2fe9ff4bd3ad31291223c5a44934b089026a5b74
iAidan <me@aidantaylor.net>
2014-06-16 00:07:21 +0100

    Added ability to give pies to another player

--- src/main/java/net/aidantaylor/custardpie/Commands/PieCommand.java
+++ src/main/java/net/aidantaylor/custardpie/Commands/PieCommand.java
@@ -1,6 +1,7 @@
 package net.aidantaylor.custardpie.commands;
 
 import net.aidantaylor.custardpie.utils.Items;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class PieCommand implements CommandExecutor {
+	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("custardpie")) {
 			if (args.length < 1) {
@@ -18,18 +20,47 @@ public class PieCommand implements CommandExecutor {
 				} else {
 					sender.sendMessage(ChatColor.RED + "You don't have permission for this command!");
 				}
-			} else if (sender.hasPermission("custardpie.spawn.multi")) {
-				int amount = Integer.parseInt(args[0]);
-				
-				if (amount < 1) {
-					sender.sendMessage(ChatColor.RED + "You cant have " + amount + " pies!");
-				} else {
-					givePie((Player) sender, amount);
+			} else if (args.length < 3) {
+				if (isInteger(args[0]) && sender instanceof Player && (sender.hasPermission("custardpie.spawn.multi") || sender.isOp())) {
+					int amount = Integer.parseInt(args[0]);
 					
-					if (amount == 1) {
-						sender.sendMessage(ChatColor.GREEN + "Enjoy your custard pie!");
+					if (amount < 1) {
+						sender.sendMessage(ChatColor.RED + "You cant have " + amount + " pies!");
 					} else {
-						sender.sendMessage(ChatColor.GREEN + "Enjoy your " + amount + " custard pies!");
+						givePie((Player) sender, amount);
+						
+						if (amount == 1) {
+							sender.sendMessage(ChatColor.GREEN + "Enjoy your custard pie!");
+						} else {
+							sender.sendMessage(ChatColor.GREEN + "Enjoy your " + amount + " custard pies!");
+						}
+					}
+				} else {
+					Player recipient = (Player) Bukkit.getServer().getPlayer(args[0]);
+					
+					if (args.length == 2 && (isInteger(args[1]) && sender.hasPermission("custardpie.spawn.multi.other"))) {
+						int amount = Integer.parseInt(args[1]);
+						
+						if (amount < 1) {
+							sender.sendMessage(ChatColor.RED + "You cant give " + amount + " pies to " + ChatColor.DARK_RED + args[0] + ChatColor.RED + "!");
+						} else if(recipient != null){
+							if (amount == 1) {
+								givePie(recipient);
+								sender.sendMessage(ChatColor.DARK_GREEN + sender.getName() + ChatColor.GREEN + " gave you a custard pie!");
+							} else {
+								givePie(recipient, amount);
+								sender.sendMessage(ChatColor.DARK_GREEN + sender.getName() + ChatColor.GREEN + " gave you " + amount + " custard pies!");
+							}
+						} else {
+							sender.sendMessage(ChatColor.DARK_RED + args[0] + ChatColor.RED + " is offline!");
+						}
+					} else if (sender.hasPermission("custardpie.spawn.other")) {
+						if(recipient != null){
+							givePie(recipient);
+							sender.sendMessage(ChatColor.DARK_GREEN + sender.getName() + ChatColor.GREEN + " gave you a custard pie!");
+						} else {
+							sender.sendMessage(ChatColor.DARK_RED + args[0] + ChatColor.RED + " is offline!");
+						}
 					}
 				}
 			} else {
@@ -48,5 +79,14 @@ public class PieCommand implements CommandExecutor {
 		ItemStack c = Items.custardPie.clone();
 		c.setAmount(amount);
 		player.getInventory().addItem(new ItemStack[] { c });
+	}
+	
+	public static boolean isInteger(String s) {
+	    try {
+	        Integer.parseInt(s);
+	        return true;
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    }
 	}
 }
